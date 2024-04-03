7a6ec9663bf59f8885fa76e6b8c7cde85b0c40d5
Rumaiseciq <165875486+Rumaiseciq@users.noreply.github.com>
2024-04-03 16:15:52 +0530

    Update Assignment5.java

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
@@ -55,19 +55,19 @@ public class Assignment5 extends AssignmentEndpoint {
       return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
     }
     try (var connection = dataSource.getConnection()) {
-      PreparedStatement statement =
-          connection.prepareStatement(
-              "select password from challenge_users where userid = '"
-                  + username_login
-                  + "' and password = '"
-                  + password_login
-                  + "'");
-      // var statement =
-      // connection.prepareStatement(
-      // "select password from sql_challenge_users where userid = ? and password =
-      // ?");
-      // statement.setString(1, username_login);
-      // statement.setString(2, password_login);
+      // PreparedStatement statement =
+      //     connection.prepareStatement(
+      //         "select password from challenge_users where userid = '"
+      //             + username_login
+      //             + "' and password = '"
+      //             + password_login
+      //             + "'");
+      var statement =
+      connection.prepareStatement(
+      "select password from sql_challenge_users where userid = ? and password =
+      ?");
+      statement.setString(1, username_login);
+      statement.setString(2, password_login);
       ResultSet resultSet = statement.executeQuery();
 
       if (resultSet.next()) {
